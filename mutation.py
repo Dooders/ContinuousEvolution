36debbb678b6ccb4510196c00289ccfe53b2cfe0
Chris Mangum <csmangum@gmail.com>
2024-05-31 14:49:28 -0700

    updated

--- mutation.py
+++ mutation.py
@@ -2,55 +2,123 @@ from abc import ABC, abstractmethod
 from typing import Tuple
 
 import torch
-import torch.nn as nn
+
+from agent import Agent
 
 
 class MutationStrategy(ABC):
+    """
+    Abstract base class for mutation strategies.
+
+    Intended to be used as a base class for implementing different mutation
+    strategies.
+
+    Methods
+    -------
+    mutate(agent: "Agent") -> "Agent":
+        Perform mutation on the weights of the neural network.
+    """
+
+    @classmethod
     @abstractmethod
-    def mutate(self, network: nn.Module):
-        pass
+    def mutate(cls, agent: "Agent") -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
+
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        raise NotImplementedError("Mutation strategy must implement the mutate method.")
 
 
-class UniformMutation(MutationStrategy):
+class Uniform(MutationStrategy):
     """
     Each weight of the neural network has a fixed probability of being altered.
 
     The new value is typically chosen randomly from a uniform distribution over
     a predefined range. This is a straightforward and widely used mutation strategy.
+
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(self, mutation_rate: float = 0.1, scale: float = 0.05) -> None:
-        self.mutation_rate = mutation_rate
-        self.scale = scale
+    def mutate(
+        cls, agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate:
-                noise = torch.rand_like(param) * self.scale
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        scale: float, optional
+            Range of the uniform distribution from which the new value is chosen,
+            by default 0.05
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
+                noise = torch.rand_like(param) * scale
                 param.data += noise
 
 
-class GaussianMutation(MutationStrategy):
+class Gaussian(MutationStrategy):
     """
     Strategy involves adding a small change to the weights, where the change
     follows a Gaussian (or normal) distribution.
 
     This allows for both small and occasionally larger tweaks in the weights,
     facilitating both fine and coarse tuning of the neural network.
+
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(self, mutation_rate: float = 0.1, scale: float = 0.05) -> None:
-        self.mutation_rate = mutation_rate
-        self.scale = scale
+    def mutate(
+        cls, agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate:
-                noise = torch.randn_like(param) * self.scale
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        scale: float, optional
+            Standard deviation of the Gaussian distribution, by default 0.05
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
+                noise = torch.randn_like(param) * scale
                 param.data += noise
 
 
-class NonUniformMutation(MutationStrategy):
+class NonUniform(MutationStrategy):
     """
     Mutation varies over time, becoming more fine-grained as the number of
     generations increases.
@@ -59,73 +127,127 @@ class NonUniformMutation(MutationStrategy):
     broader search space, and later it fine-tunes the solutions by making
     smaller changes.
 
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05, max_generations: int = 1000) -> "Agent":
+        Perform mutation on the weights of the neural network.
+
     #! Not working
     """
 
-    def __init__(
-        self,
+    def mutate(
+        cls,
+        agent: "Agent",
         mutation_rate: float = 0.1,
         scale: float = 0.05,
         max_generations: int = 1000,
-    ) -> None:
-        self.mutation_rate = mutation_rate
-        self.scale = scale
-        self.max_generations = max_generations
-        self.generation = 0
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate * (
-                1 - self.generation / self.max_generations
-            ):
-                noise = torch.randn_like(param) * self.scale
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        scale: float, optional
+            Range of the uniform distribution from which the new value is chosen,
+            by default 0.05
+        max_generations: int, optional
+            Maximum number of generations, by default 1000
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        generation = 0
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate * (1 - generation / max_generations):
+                noise = torch.randn_like(param) * scale
                 param.data += noise
 
-        self.generation += 1
+        generation += 1
 
 
-class PolynomialMutation(MutationStrategy):
+class Polynomial(MutationStrategy):
     """
     Provides a way to control the distribution of mutations, allowing the
     algorithm to fine-tune solutions more effectively.
 
     It uses a polynomial probability distribution to decide the magnitude of mutation.
+
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(
-        self, mutation_rate: float = 0.1, scale: float = 0.05, eta: float = 20
-    ) -> None:
-        self.mutation_rate = mutation_rate
-        self.scale = scale
-        self.eta = eta
+    def mutate(
+        cls, agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate:
-                noise = torch.randn_like(param) * self.scale
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        scale: float, optional
+            Range of the uniform distribution from which the new value is chosen,
+            by default 0.05
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
+                noise = torch.randn_like(param) * scale
                 delta = noise * (2 * torch.rand_like(param) - 1)
                 param.data += delta
 
 
-class BitFlipMutation(MutationStrategy):
+class BitFlip(MutationStrategy):
     """
     Solutions are encoded in binary, bit flip mutation can be adapted for neural
     networks by considering a binary representation of the weights.
 
     Each bit has a probability of being flipped (changed from 0 to 1, or vice versa).
+
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(self, mutation_rate: float = 0.1) -> None:
-        self.mutation_rate = mutation_rate
+    def mutate(cls, agent: "Agent", mutation_rate: float = 0.1) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate:
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
                 mask = torch.randint(0, 2, param.size(), device=param.device)
                 param.data = param.data ^ mask
 
 
-class BoundaryMutation(MutationStrategy):
+class Boundary(MutationStrategy):
     """
     Strategy specifically targets the boundaries of the parameter space.
 
@@ -134,30 +256,50 @@ class BoundaryMutation(MutationStrategy):
 
     This can be particularly useful when the optimal parameters are suspected to
     lie near the edges of the parameter range.
+
+    Methods
+    -------
+    mutate(agent: "Agent", mutation_rate: float = 0.1, upper_bound: float = 1.0, lower_bound: float = -1.0) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(
-        self,
+    def mutate(
+        cls,
+        agent: "Agent",
         mutation_rate: float = 0.1,
-        lower_bound: float = -1.0,
         upper_bound: float = 1.0,
-    ) -> None:
-        self.mutation_rate = mutation_rate
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+        lower_bound: float = -1.0,
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.mutation_rate:
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        upper_bound: float, optional
+            Upper boundary value, by default 1.0
+        lower_bound: float, optional
+            Lower boundary value, by default -1.0
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
                 mask = torch.rand_like(param) < 0.5
                 param.data = torch.where(
                     mask,
-                    torch.tensor(self.lower_bound, device=param.device),
-                    torch.tensor(self.upper_bound, device=param.device),
+                    torch.tensor(lower_bound, device=param.device),
+                    torch.tensor(upper_bound, device=param.device),
                 )
 
 
-class AdaptiveGaussianMutation(MutationStrategy):
+class AdaptiveGaussian(MutationStrategy):
     """
     Adjust the mutation parameters dynamically based on the performance of the
     population across generations.
@@ -165,6 +307,13 @@ class AdaptiveGaussianMutation(MutationStrategy):
     The idea is to increase mutation rates when the population appears to be
     converging prematurely (to escape local minima) and to decrease them as the
     solutions approach an optimum to maintain stability.
+
+    Methods
+    -------
+    update_rate(current_performance) -> None:
+        Update the mutation rate based on the current performance.
+    mutate(agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
     def __init__(
@@ -187,36 +336,78 @@ class AdaptiveGaussianMutation(MutationStrategy):
             self.rate = min(self.max_rate, self.rate * 1.1)
         self.performance_tracker.append(current_performance)
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.rate:
-                noise = torch.randn_like(param) * self.scale
+    def mutate(
+        cls, agent: "Agent", mutation_rate: float = 0.1, scale: float = 0.05
+    ) -> "Agent":
+        """
+        #! Needs to be finished
+        Perform mutation on the weights of the neural network.
+
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        mutation_rate: float, optional
+            Probability of a weight being mutated, by default 0.1
+        scale: float, optional
+            Standard deviation of the Gaussian distribution, by default 0.05
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+
+        for param in agent.parameters():
+            if torch.rand(1) < mutation_rate:
+                noise = torch.randn_like(param) * scale
                 param.data += noise
 
 
-class HybridMutation(MutationStrategy):
+class Hybrid(MutationStrategy):
     """
     Combine different mutation mechanisms to take advantage of the benefits of each.
 
     For example, you might use both Gaussian and uniform mutations, where Gaussian
     provides small, fine-tuned changes, and uniform allows for occasional large jumps.
+
+    Methods
+    -------
+    mutate(agent: "Agent", rate_gaussian: float = 0.1, scale_gaussian: float = 0.05, rate_uniform: float = 0.1, range_uniform: Tuple[float, float] = (-0.1, 0.1)) -> "Agent":
+        Perform mutation on the weights of the neural network.
     """
 
-    def __init__(
-        self,
+    def mutate(
+        cls,
+        agent: "Agent",
         rate_gaussian: float = 0.1,
         scale_gaussian: float = 0.05,
         rate_uniform: float = 0.1,
         range_uniform: Tuple[float, float] = (-0.1, 0.1),
-    ) -> None:
-        self.rate_gaussian = rate_gaussian
-        self.scale_gaussian = scale_gaussian
-        self.rate_uniform = rate_uniform
-        self.range_uniform = range_uniform
+    ) -> "Agent":
+        """
+        Perform mutation on the weights of the neural network.
 
-    def mutate(self, network: nn.Module) -> None:
-        for param in network.parameters():
-            if torch.rand(1) < self.rate_gaussian:
-                param.data += self.scale_gaussian * torch.randn_like(param)
-            if torch.rand(1) < self.rate_uniform:
-                param.data += torch.empty_like(param).uniform_(*self.range_uniform)
+        Parameters
+        ----------
+        agent: "Agent"
+            Neural network to be mutated.
+        rate_gaussian: float, optional
+            Probability of a weight being mutated by Gaussian noise, by default 0.1
+        scale_gaussian: float, optional
+            Standard deviation of the Gaussian distribution, by default 0.05
+        rate_uniform: float, optional
+            Probability of a weight being mutated by uniform noise, by default 0.1
+        range_uniform: Tuple[float, float], optional
+            Range of the uniform distribution from which the new value is chosen, by default (-0.1, 0.1)
+
+        Returns
+        -------
+        "Agent"
+            Neural network with mutated weights.
+        """
+        for param in agent.parameters():
+            if torch.rand(1) < rate_gaussian:
+                param.data += scale_gaussian * torch.randn_like(param)
+            if torch.rand(1) < rate_uniform:
+                param.data += torch.empty_like(param).uniform_(*range_uniform)
