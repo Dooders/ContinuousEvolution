f880139cdccc2449d98fb17f1ecc618218c2a206
Chris Mangum <csmangum@gmail.com>
2024-09-28 17:25:33 -0700

    changed scaler

--- evolution/data/price.py
+++ evolution/data/price.py
@@ -1,29 +1,26 @@
-import json
 import os
 
 import numpy as np
 import pandas as pd
-from data import Dataset
 from sklearn.discriminant_analysis import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+from evolution.data.data import Dataset
 
 
-def process_json_to_csv(json_file: str) -> None:
-    """
-    Process a JSON file to a CSV file.
+def process_bitcoin_csv() -> None:
+    """ """
 
-    Parameters
-    ----------
-    json_file : str
-        The path to the JSON file.
-    """
-    with open(json_file, "r") as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
+    df = pd.read_csv("evolution/data/sample_bitcoin.csv")
 
+    # Select only numeric columns for scaling
+    numeric_columns = df.select_dtypes(include=[np.number]).columns
+
+    # Scale only the numeric columns
+    scaler = MinMaxScaler()
     df_scaled = df.copy()
-    df_scaled = df_scaled.drop(columns=["Timestamp"])
-    scaler = StandardScaler()
-    df_scaled[df_scaled.columns] = scaler.fit_transform(df_scaled[df_scaled.columns])
+    df_scaled[numeric_columns] = scaler.fit_transform(df[numeric_columns])
+
     df_scaled.to_csv("evolution/data/price_df.csv", index=False)
 
 
@@ -37,8 +34,7 @@ def create_dataset() -> np.ndarray:
         The dataset.
     """
     df = pd.read_csv("evolution/data/price_df.csv")
-
-    return df.to_numpy()
+    return df
 
 
 def load_dataset() -> Dataset:
@@ -51,15 +47,16 @@ def load_dataset() -> Dataset:
         The dataset.
     """
     if not os.path.exists("evolution/data/price_df.csv"):
-        process_json_to_csv("evolution/data/price_dict.json")
+        process_bitcoin_csv()
 
-    metadata = {
-        "features": df.columns.tolist(),
-        "target_index": 0,
-    }
     df = create_dataset()
 
-    return Dataset(df, np.arange(len(df)), metadata, 0)
+    indices = df["Timestamp"].to_numpy()
+    final_df = df.drop(columns=["Timestamp"])
+    metadata = {
+        "features": final_df.columns.tolist(),
+    }
+    return Dataset(final_df.to_numpy(), indices, metadata, target_index=0)
 
 
 bitcoin_dataset = load_dataset()
